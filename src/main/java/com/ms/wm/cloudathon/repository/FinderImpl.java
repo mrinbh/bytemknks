55e27ed9b03367d9088c8bc853f3b584c92bc77c
mrinab <mrinal.bhardwaj@lab.morganstanley.com>
2022-02-25 12:31:49 +0000

    Cloud: Add azure app Insight

--- src/main/java/com/ms/wm/cloudathon/repository/FinderImpl.java
+++ src/main/java/com/ms/wm/cloudathon/repository/FinderImpl.java
@@ -1,5 +1,7 @@
 package com.ms.wm.cloudathon.repository;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.MetricTelemetry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -7,12 +9,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class FinderImpl {
 
+    @Autowired
+    TelemetryClient telemetryClient;
+
     public static final String SELECT_ID_EMP_FNAME_EMP_LNAME_FROM_BYTE_THE_CLOUD_T_1 = "SELECT id,emp_fname,emp_lname from byte_the_cloud_t1";
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public void test(){
-       jdbcTemplate.query(SELECT_ID_EMP_FNAME_EMP_LNAME_FROM_BYTE_THE_CLOUD_T_1, (rs,rowNum)-> {
+        long startTime = System.nanoTime();
+        jdbcTemplate.query(SELECT_ID_EMP_FNAME_EMP_LNAME_FROM_BYTE_THE_CLOUD_T_1, (rs,rowNum)-> {
            System.out.println("Record - "+rowNum);
            System.out.println("ID - "+rs.getInt(1));
            System.out.println("EMP_FNAME - "+rs.getString(2));
@@ -20,5 +26,13 @@ public class FinderImpl {
 
            return rs.getInt(1)+rs.getString(2)+rs.getString(1);
        });
+        long endTime = System.nanoTime();
+        trackDbQuery(endTime-startTime);
+    }
+    private void trackDbQuery(long timeTaken){
+        MetricTelemetry dbQueryTime = new MetricTelemetry();
+        dbQueryTime.setName("eb_query_time");
+        dbQueryTime.setValue(timeTaken);
+        telemetryClient.trackMetric(dbQueryTime);
     }
 }
--- src/main/java/com/ms/wm/cloudathon/web/controller/RESTServiceController.java
+++ src/main/java/com/ms/wm/cloudathon/web/controller/RESTServiceController.java
@@ -1,7 +1,13 @@
 package com.ms.wm.cloudathon.web.controller;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.logback.ApplicationInsightsAppender;
+import com.microsoft.applicationinsights.telemetry.Telemetry;
 import com.ms.wm.cloudathon.model.IncomingModel;
 import com.ms.wm.cloudathon.repository.FinderImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +16,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/byteTheCloud")
 public class RESTServiceController {
 
+    private Logger logger = LoggerFactory.getLogger(RESTServiceController.class);
+
     @Autowired
     FinderImpl finder;
+
+    @Autowired
+    TelemetryClient telemetryClient;
 
     @RequestMapping(method = RequestMethod.POST,
             path ="/accept" ,
@@ -19,7 +30,25 @@ public class RESTServiceController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String acceptCall(@RequestBody IncomingModel model) {
-        finder.test();
-        return model.getSequence() + model.getTimestamp();
+        try {
+            logger.info("Incoming Model is : {}", model);
+            logger.trace("Tracing Incoming Model is : {}", model);
+            logger.error("Tracing Incoming Model is : {}", model);
+            finder.test();
+            return model.getSequence() + model.getTimestamp();
+        }catch (Exception ex){
+            telemetryClient.trackEvent("Exception  while processing model" + model);
+            telemetryClient.trackTrace(ex.getMessage());
+            return "Error while processing request" + model;
+        }
+    }
+
+    @GetMapping("/ping")
+    public String getPingFromByteTheCloudService() {
+        logger.info("Hello Trace");
+        //TEST MDC
+        MDC.put("TestKey", "value");
+        telemetryClient.trackEvent("ping_event");
+        return "Hello There, calling from Byte the Cloud Ping";
     }
 }
